1e58654cc11e20add49eb45c565f561926601d8a
lockemind <pedr.amaral@gmail.com>
2015-04-21 09:03:26 +0100

    95% complete, missing some edge cases

--- enumerable.rb
+++ enumerable.rb
@@ -58,5 +58,59 @@ module Enumerable
   end
 
 
+  def my_count(arg = nil)
 
+    count = 0
+    for v in self
+      if block_given?
+        count += 1 if yield v
+      elsif arg == nil
+        count += 1
+      else
+        count += 1 if v == arg
+      end
+    end
+    count
+  end
+
+  def my_map
+    return self unless block_given?
+
+    l = []
+    for v in self
+      l << yield(v)
+    end
+    l
+  end
+
+  # missing the feature that accepts a symbol
+  def my_inject(arg = 0)
+    self.my_each do |n|
+      arg = (yield arg, n)
+    end
+    arg
+  end
+
+   # Third iteration of my_map, method accepts a proc or a block but only executes
+   # if both are provided, otherwise the default enum is returned. The specification
+   # was ambiguous about the exact interaction of the block & proc, so I simply nested
+   # them. credit to: ThothLogos
+   def my_map2(proc = nil)
+
+      result = []
+
+      if !proc.nil? && block_given?
+         for i in self
+            result << proc.call(yield(i)); end
+         return result; end
+
+      return self.to_enum
+   end
 end
+
+def multiply_els arg
+  arg.my_inject(1) { |memo,v|
+    memo * v
+  }
+end
+
--- enumerable_spec.rb
+++ enumerable_spec.rb
@@ -84,3 +84,93 @@ describe "#my_none?" do
   end
 end
 
+
+
+
+
+describe "#my_count" do
+  it "#my_count 1 - should return the number of items in enum through enumeration." do
+    expect(test_array1.my_count).to eq(3)
+  end
+end
+
+describe "#my_count" do
+  it "#my_count 2.1 - should if an argument is given, the number of items in enum that are equal to item are counted." do
+    expect(test_array1.my_count(6)).to eq(1)
+  end
+end
+
+
+describe "#my_count" do
+  it "#my_count 2.2 - should if an argument is given, the number of items in enum that are equal to item are counted." do
+    expect(test_array2.my_count(6)).to eq(0)
+  end
+end
+
+describe "#my_count" do
+  it "#my_count 3.1 - should If a block is given, it counts the number of elements yielding a true value." do
+    expect(test_array1.my_count { |v| v > 0 }).to eq(3)
+  end
+end
+
+describe "#my_count" do
+  it "#my_count 3.2 - should If a block is given, it counts the number of elements yielding a true value." do
+    expect(test_array2.my_count { |v| v > 0 }).to eq(2)
+  end
+end
+
+describe "#my_map" do
+  it "#my_map 1 - should Return a new array with the results of running block once for every element in enum." do
+    expect(test_array1.my_map { |v| v + 1}).to eq([3,5,7])
+  end
+end
+
+describe "#my_map" do
+  it "#my_map 2 - should If no block is given, an enumerator is returned instead." do
+    expect(test_array1.my_map).to eq([2,4,6])
+  end
+end
+
+# describe "#my_inject" do
+#   it "#my_inject 1 - Combines all elements of enum by applying a binary operation, specified by a block or a symbol that names 
+# a method or operator." do
+#     expect(test_array1.my_inject(:+)).to eq(12)
+#   end
+# end
+
+describe "#my_inject" do
+  it "#my_inject 2 - If you specify a block, then for each element in enum the block is passed an accumulator value (memo) 
+and the element." do
+    expect(test_array1.my_inject {|memo, v| memo += v}).to eq(12)
+  end
+end
+
+describe "#my_inject" do
+  it "#my_inject 3" do
+    expect(test_array1.my_inject(10) {|memo, v| memo += v}).to eq(22)
+  end
+end
+
+# describe "#my_inject" do
+#   it "#my_inject 4" do
+#     expect(test_array1.my_inject(10, :+)).to eq(22)
+#   end
+# end
+
+describe "#my_inject" do
+  it "#my_inject 5" do
+    expect(multiply_els([2,4,5])).to eq(40)
+  end
+end
+
+describe "#my_map2" do
+  it "#my_map 1 - should Return a new array with the results of running block once for every element in enum." do
+    expect(test_array1.my_map2(Proc.new {|v| v + 1 }) { |v| v + 1}).to eq([4,6,8])
+  end
+end
+
+describe "#my_map2" do
+  it "#my_map 2 - should If no block is given, an enumerator is returned instead." do
+    expect(test_array1.my_map2.to_a	).to eq([2,4,6])
+  end
+end
